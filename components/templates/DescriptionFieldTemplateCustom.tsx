352489ff3e3f1ce56ffc1f7b80c0294b69d0f351
Ykrop3000 <grisha.andreev.03@gmail.com>
2024-12-23 06:25:54 +0300

    add customer support

---
+++ components/templates/DescriptionFieldTemplateCustom.tsx
@@ -0,0 +1,15 @@
+// import React from 'react';
+// import { DescriptionFieldProps, RJSFSchema } from '@rjsf/utils'; // Importing the type for props
+// import { Card, CardHeader, InfoLabel, makeStyles, Text } from '@fluentui/react-components'; // Importing UI components
+// import { getDefaultRegistry } from '@rjsf/core';
+
+// const registry = getDefaultRegistry();
+// const DescriptionFieldTemplate = registry.templates.DescriptionFieldTemplate
+
+// const DescriptionFieldTemplateCustom = (props: DescriptionFieldProps) => {
+//     return <InfoLabel
+//         info={props.description}
+//     />
+
+// }
+// export default DescriptionFieldTemplateCustom
---
+++ types/customer.ts
@@ -0,0 +1,7 @@
+export interface Customer {
+    id: string
+    name: string
+    cellPhone: string
+    first_name: string
+    last_name: string
+}
